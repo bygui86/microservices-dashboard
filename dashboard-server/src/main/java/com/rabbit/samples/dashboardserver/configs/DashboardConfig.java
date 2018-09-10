7f4b038ce254edca29dff8c09dc5fa2a148f7019
Matteo Baiguini <baiguini.matteo@gmail.com>
2018-09-10 07:58:23 +0200

    Added microservices-dashboard server

---
+++ dashboard-server/src/main/java/com/rabbit/samples/dashboardserver/configs/DashboardConfig.java
@@ -0,0 +1,12 @@
+package com.rabbit.samples.dashboardserver.configs;
+
+import be.ordina.msdashboard.EnableMicroservicesDashboardServer;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration("dashboardConfig")
+@EnableMicroservicesDashboardServer
+public class DashboardConfig {
+
+	// no-op
+}
